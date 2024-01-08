8acf6ca5189f834335ee0d5482a1f75d997d9f5f
Khobe <chuksleo27@gmail.com>
2024-01-08 18:00:30 +0100

    Space Commit

---
+++ my-project/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-black bottom-0 relative justify-center'>
+   <p className='text-white text-center'>SpaceX &copy; 2024</p>
+   <div className='flex flex-row justify-center space-x-4'>
+    <h1 className='text-white text-center font-bold'>PRIVACY POLICY</h1>
+    <h1 className='text-white text-center font-bold'>SUPPLIERS</h1>
+   </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ my-project/src/components/RocketFold.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RocketFold = ({rocket}) => {
+ return (
+  <div>
+ {rocket.id}
+  </div>
+ )
+}
+
+export default RocketFold
